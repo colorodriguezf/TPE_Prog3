ec90634ffe8d699c36b34138082503ae466042f0
colorodriguezf <colitorodriguez97@gmail.com>
2023-05-15 12:10:32 -0300

    servicios

---
+++ 1raParte/src/ServicioBFS.java
@@ -0,0 +1,17 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioBFS {
+    private Grafo<?> grafo;
+
+    public ServicioBFS(final Grafo<?> grafo) {
+        this.grafo = grafo;
+    }
+
+    public List<Integer> bfsForest() {
+        // Resolver BFS
+        return new ArrayList<>();
+    }
+}
---
+++ 1raParte/src/ServicioDFS.java
@@ -0,0 +1,18 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioDFS {
+    
+    private Grafo<?> grafo;
+
+    public ServicioDFS(Grafo<?> grafo) {
+        this.grafo = grafo;
+    }
+
+    public List<Integer> dfsForest() {
+        // Resolver DFS
+        return new ArrayList<>();
+    }
+}
